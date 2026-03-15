bec4c5be9e2527a54e3dc6c8b8c163d88d916274
SravaniMethukul <methukulasravani14@gmail.com>
2026-03-15 16:54:57 +0100

    updated config file

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ const config = ({
   use: {
 
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: 'on', //screenshot for everystep (only-on-failure, on, off)
     trace: 'on', // log information for each step(on, off, retain-on-failure)
   },
